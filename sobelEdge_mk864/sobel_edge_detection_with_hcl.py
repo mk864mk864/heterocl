5d5f65fd88e5281a657585803f54a900f5ee78c9
Mira Kim <mk864@zhang-x1.ece.cornell.edu>
2020-03-20 00:12:28 -0400

    added comments

--- sobelEdge_mk864/sobel_edge_detection_with_hcl.py
+++ sobelEdge_mk864/sobel_edge_detection_with_hcl.py
@@ -1,37 +1,33 @@
 
-#Import Modules
+#import modules
 import heterocl as hcl
 from PIL import Image
 import math
 import os
-import PIL
 import numpy as np
 from scipy import misc
 
+#need to initiate hcl
 hcl.init(init_dtype=hcl.Float())
 
+#specify width and height of the input image
 width = 225
 height = 225
 
-path = 'spongebob.png'
-img = misc.imread(path)
-imgdd = np.zeros((height,width,3))
-for x in range (0,height):
-	for y in range (0,width):
-		for z in range (0,3):
-			imgdd[x, y, z] = img[x,y,z]
-
-imgdata = hcl.asarray(imgdd)
+#need to define placeholders to define kernel and create schedule
 data = hcl.placeholder((width, height, 3), "data", dtype=hcl.Float())
 Gx = hcl.placeholder((3,3), "Gx", dtype=hcl.Float())
 Gy = hcl.placeholder((3,3), "Gy", dtype=hcl.Float())
 
-Gxdata1 = hcl.asarray(np.array([[1,0,-1],[2,0,-2],[1,0,-1]]))
-Gydata1 = hcl.asarray(np.array([[1,2,1],[0,0,0],[-1,-2,-1]]))
+#path to input image
+path = 'spongebob.png'
 
-Gxdata = hcl.asarray(Gxdata1)
-Gydata = hcl.asarray(Gydata1)
+#need to convert all input data to hcl arrays
+imgdata = hcl.asarray(np.asarray(misc.imread(path)))
+Gxdata = hcl.asarray(np.array([[1,0,-1],[2,0,-2],[1,0,-1]]))
+Gydata = hcl.asarray(np.array([[1,2,1],[0,0,0],[-1,-2,-1]]))
 
+#define kernel for all computations done in hcl
 def kernel(data, Gx, Gy):
 	newdata =  hcl.compute((width, height), lambda x,y: data[x][y][0] + data[x][y][1] + data[x][y][2], "newdata", dtype=hcl.Float()) 
 	r = hcl.reduce_axis(0,3)
@@ -42,20 +38,27 @@ def kernel(data, Gx, Gy):
 	Gyresult = hcl.compute((width-2, height-2), lambda x,y: hcl.sum(newdata[x+t, y+g]*Gy[t,g], axis=[t,g]), "Gyresult", dtype=hcl.Float())
 	return hcl.compute((width-2, height-2), lambda x,y: hcl.sqrt((Gxresult[x][y]*Gxresult[x][y])+(Gyresult[x][y]*Gyresult[x][y]))/4328*255, dtype=hcl.Float())
 
-
+#create schedule and function
 sched = hcl.create_schedule([data, Gx, Gy], kernel)
 func = hcl.build(sched)
 
+#need to define output array as an hcl array
 length = hcl.asarray(np.zeros((width-2, height-2)) ,dtype=hcl.Float())
 
+#run the function
 func(imgdata, Gxdata, Gydata, length)
+
+#change the type of output back to numpy array
 newlength = length.asnumpy().astype(int)
 
+#define array for image
 newimgarry = np.zeros((width-2,height-2,3))
 
+#assign (length, length, length) to each pixel
 for x in range (0, width-2):
 	for y in range (0, height-2):
 		for z in range (0, 3):
 			newimgarry[x,y,z] = newlength[x,y]
 
+#create an image with the array
 misc.imsave('spongebob_fixed.png', newimgarry)
